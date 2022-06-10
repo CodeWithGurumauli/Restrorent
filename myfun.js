fee9f9713d5758bc6e8c5416faf3ef4dfb8529ec
CodeWithGurumauli <97522753+CodeWithGurumauli@users.noreply.github.com>
2022-06-10 06:25:02 -0400

    No Changes

---
+++ myfun.js
@@ -0,0 +1,10 @@
+let myAlert = ()=>{    
+    let a = "Order Placed Successfully....! " 
+    document.querySelector("h2").innerHTML=a;
+   
+}
+let my_Alert = ()=>{    
+    let b = "Your Feedback Submitted Successfully....!" 
+    document.querySelector("h2").innerHTML=b;
+    
+}
